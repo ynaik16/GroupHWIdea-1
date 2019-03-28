8b8f5e173f05c4dc8bfec679c9b243c09cfc1620
Franz Diesta <franzdiesta@gmail.com>
2019-03-27 17:43:07 -0700

    recommit

---
+++ app/src/main/java/com/example/test/DatabaseHelper.java
@@ -0,0 +1,60 @@
+package com.example.test;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    /* Names for database and table */
+    public static final String DATABASE_NAME = "mycontacts.db";
+    public static final String TABLE_NAME = "mycontacts_data";
+
+    /* naming columns in SQLite Database */
+    public static final String COL1 = "ID";
+    public static final String COL2 = "NAME";
+    public static final String COL3 = "PHONE";
+    public static final String COL4 = "EMAIL";
+
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                " NAME TEXT, PHONE TEXT, EMAIL TEXT)";
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP IF TABLE EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean addData(String fName, String lName, String fFood) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL2, fName);
+        contentValues.put(COL3, lName);
+        contentValues.put(COL4, fFood);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        if (result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public Cursor getListContents() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor data = db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+        return data;
+    }
+}
--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.test;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity  {
     Button btnAddContact;
     ListView lvContacts;
     ArrayList<ContactInfo> list;
+    DatabaseHelper myDb;
 
     //variables to hold values
     String name = "", phone = "", email = "";
@@ -34,7 +36,7 @@ public class MainActivity extends AppCompatActivity  {
         setContentView(R.layout.activity_main);
 
         /* Incomplete/ Test variables */
-        final Dialog  MyDialog = new Dialog(this);
+        final Dialog MyDialog = new Dialog(this);
 
         /* References to the components on the app */
         btnAddContact = findViewById(R.id.btnXMLAddContact);
@@ -42,6 +44,26 @@ public class MainActivity extends AppCompatActivity  {
 
         /* Holds ContactInfo (name,phone, email) */
         list = new ArrayList<ContactInfo>();
+
+        /* Use Database to load in on the initialization of app */
+        myDb = new DatabaseHelper(this);
+        Cursor data = myDb.getListContents();
+
+        if(data.getCount() > 0) {
+            while(data.moveToNext()) {
+                //get the string in each column and add it in a new ContactInfo person
+                String dataName = data.getString(1);
+                String dataPhone = data.getString(2);
+                String dataEmail = data.getString(3);
+                ContactInfo person = new ContactInfo(dataName,dataPhone,dataEmail);
+
+                //add it to the list
+                list.add(person);
+                ContactAdapter adapter = new ContactAdapter(this, list);
+                lvContacts.setAdapter(adapter);
+            }
+        }
+
 
         //Called when the 'Add Contact' button is clicked/tapped
         btnAddContact.setOnClickListener(new View.OnClickListener() {
@@ -58,7 +80,7 @@ public class MainActivity extends AppCompatActivity  {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 /* In words saying 'in the list of contacts, get the array position(i), and return whatever
-                * Basically want o use these to pass into an Intent*/
+                 * Basically want o use these to pass into an Intent*/
                 String conName = list.get(i).getName();
                 final String conPhone = list.get(i).getPhone();
                 final String conEmail = list.get(i).getEmail();
@@ -107,7 +129,7 @@ public class MainActivity extends AppCompatActivity  {
 
     //Called when you successfully return from your Intent/startActivity
     @Override
-       protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         //If you used the requestCode and successfully returned
@@ -122,6 +144,9 @@ public class MainActivity extends AppCompatActivity  {
                 ContactInfo newContact = new ContactInfo(name,phone,email);
                 list.add(newContact);
 
+                //for database
+                AddData(name,phone,email);
+
                 //Create an adapter to speak to the ListView
                 //Pass context of this page and the list populated with returned Intent data
                 ContactAdapter adapter = new ContactAdapter(this, list);
@@ -130,5 +155,12 @@ public class MainActivity extends AppCompatActivity  {
                 lvContacts.setAdapter(adapter);
             }
         }
+    }
+
+    public void AddData(String item1, String item2, String item3){
+        boolean insertData = myDb.addData(item1,item2,item3);
+
+        if (insertData == false)
+            Toast.makeText(MainActivity.this,"Failed to enter data to database" , Toast.LENGTH_LONG).show();
     }
 }
